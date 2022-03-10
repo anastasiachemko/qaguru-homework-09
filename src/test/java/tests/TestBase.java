4b46e291173c66e26ab528ec4f9134d968e6a861
Anastasia Chemko <nast.chemko@gmail.com>
2022-03-10 23:53:37 +0300

    add owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,34 +2,31 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static java.lang.String.format;
 
 public class TestBase {
 
     @BeforeAll
     static void setUp() {
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+        Configuration.remote = format("https://%s:%s@%s",
+                credentials.login(),
+                credentials.password(),
+                System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub"));
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
-        String browser = System.getProperty("browser", "chrome");
-        String version = System.getProperty("version", "90");
-        String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
-        String login = System.getProperty("login", "user1");
-        String password = System.getProperty("password", "1234");
-
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = browser;
-        Configuration.browserVersion = version;
-
-        String url = "https://" + login + ":" + password + "@" + remoteUrl;
-
-        Configuration.remote = url;
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "90");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
